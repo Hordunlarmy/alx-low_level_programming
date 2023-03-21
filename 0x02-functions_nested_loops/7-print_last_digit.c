abecc17a41b5ab30fa9ba260001f6193c4a84c1b
Hordunlarmy <Hordunlarmy@gmail.com>
2023-03-21 20:36:44 +0000

    Add function to print the 9 times table in a formatted way

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,13 +3,17 @@
 
 /**
  * print_last_digit - Entry point
- * @x: integer to be computed
- * Return: last digit of a number.
+ * @n: number to be computed
+ * Return: the last digit of a number.
  */
-int print_last_digit(int x)
+int print_last_digit(int n)
 {
-	int last;
+	int last = n % 10;
 
-	last = x % 10;
+	if (last < 0)
+		last *= -1;
+
+	_putchar(last + '0');
+
 	return (last);
 }
