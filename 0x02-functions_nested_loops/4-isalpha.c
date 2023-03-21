eebd1c0b8821ced888b288ca2d0bb93f03591e81
Hordunlarmy <Hordunlarmy@gmail.com>
2023-03-21 15:12:45 +0000

    Add function _isalpha to check if a character is a letter in any case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * _isalpha - Entry point
+ * @c: The character to be checked
+ * Return: 1 if c is a letter, lowercase or uppercase
+ */
+int _isalpha(int c)
+{
+	if (c <= '9' && c >= '0')
+	{
+		if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A))
+			return (1);
+	}
+	else
+		return (0);
+}
