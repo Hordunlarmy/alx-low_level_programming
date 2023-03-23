33751c6bd08db8fc1f8a032a23243163c256474b
Hordunlarmy <Hordunlarmy@gmail.com>
2023-03-23 23:34:41 +0000

    Add implementation for printing FizzBuzz to main function

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ * @n: number of times character gets printed
+ * Return: a diagonal line on the terminal.
+ */
+void print_diagonal(int n)
+{
+	int line, space;
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+		{
+			for (space = 0; space < line; space++)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (line == n - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
