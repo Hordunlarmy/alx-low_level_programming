4603b2d893c0628a6ed6caefee85ce236bbaafc2
Hordunlarmy <Hordunlarmy@gmail.com>
2023-03-23 13:12:34 +0000

    Add new files and functions for more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * mul - Entry point
+ * @a: first variable
+ * @b: second variable
+ * Return: Always 0 success
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
