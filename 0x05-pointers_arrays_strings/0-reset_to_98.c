00e82b434bcaf2e526a210ba74413b6cb125776f
Hordunlarmy <Hordunlarmy@gmail.com>
2023-03-27 15:33:52 +0000

    Add new files and functions for resetting a variable to 98 in 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+/**
+ * reset_to_98 - Entry point
+ * @n: variable used
+ * Return: a pointer to 98.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,3 @@
+/* Function prototype declarations*/
+
+void reset_to_98(int *n);
