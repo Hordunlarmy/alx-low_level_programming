47fa46abffbd0877d9c1d2778668d0c1c3a7097a
Hordunlarmy <Hordunlarmy@gmail.com>
2023-03-31 23:12:06 +0000

    Convert letters to numbers using leet code in 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,15 +8,15 @@
 char *leet(char *s)
 {
 	char *p = s;
-	char swap[] = {'a', 'e', 'o', 't', 'l'};
-	char numSwap[] = {'4', '3', '0', '7', '1'};
+	char swap[] = "aAeEoOtTlL";
+	char numSwap[] = "4433007711";
 	int i, j;
 
 	for (i = 0; *(p + i) != '\0'; i++)
 	{
 		for (j = 0; *(swap + j) != '\0'; j++)
 
-			if (*(p + i) == *(swap + j) || *(p + i) - 32 == *(swap + j))
+			if (*(p + i) == *(swap + j))
 			{
 				*(p + i) = *(numSwap + j);
 				break;
