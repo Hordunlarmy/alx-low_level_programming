977891539624a8cbf8c9e1470f75d161ed8b112b
Hordunlarmy <Hordunlarmy@gmail.com>
2023-04-03 17:19:55 +0000

    Add implementation for _strchr function with test case in 2-main.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * _strchr - Entry point
+ * @s: pointer variable
+ * @c: character to be checked
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	char *p = s;
+	int i;
+
+	for (i = 0; *(p + i) != '\0'; i++)
+
+		if (*(p + i) == c)
+		{
+			return (p + i);
+		}
+
+	return (0);
+}
