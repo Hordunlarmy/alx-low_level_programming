4e07cb3329520fdd6a3abe4ec8fcda09e0bc44c4
Hordunlarmy <Hordunlarmy@gmail.com>
2023-04-08 16:46:05 +0000

    Add new file '1-args.c' and 'nargs' which counts the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * @argc: arg count
+ * @argv: arg array
+ * Return: the number of arguments passed into it.
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+		printf("%d\n", argc - 1);
+	return (0);
+}
