99735a0fc32f1feac048658a7db6b4781566d470
Hordunlarmy <Hordunlarmy@gmail.com>
2023-04-08 17:35:28 +0000

    Add program to multiply arguments in 0x0A-argc_argv folder

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argement passed
+ * Return:  multiplication two numbers.
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int sum = 1;
+
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			sum = sum * atoi(argv[count]);
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
