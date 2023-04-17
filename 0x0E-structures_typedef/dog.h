bae4ab90d860e352b4ac817c5e271c6df3696a11
Hordunlarmy <Hordunlarmy@gmail.com>
2023-04-17 21:19:58 +0100

    Add a new C program that creates a struct variable, initializes its members, and prints them using printf

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *  struct dog - data type
+ *  @name: pointer member
+ *  @age: second member variable
+ *  @owner: third member of struct dog type
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
