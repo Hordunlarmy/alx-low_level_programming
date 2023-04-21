c5a341ab9d98d75f39f0d38c739a6640a78f7a85
Hordunlarmy <Hordunlarmy@gmail.com>
2023-04-21 01:05:53 +0100

    Add function to print a variable number of strings with a separator in between

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - Entry point
+ * @separator: string
+ * @n: num,ber of arguments
+ * Return: strings, followed by a new line.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int count;
+
+	va_start(ap, n);
+
+	for (count = 0; count < n; count++)
+	{
+		if (*(separator + count) == 0)
+			printf("(nil)");
+		printf("%s", va_arg(ap, char *));
+
+		if (count != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	va_end(ap);
+
+	printf("\n");
+}
