ba95348e8297886946f25f8e6de13576dd4b5482
Hordunlarmy <Hordunlarmy@gmail.com>
2023-04-21 02:31:24 +0100

    Add print_all function to variadic_functions.h and implement it in 3-print_all.c and 3-main.c to print any arguments based on the provided format string

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - Entry point
+ * @format: argument list
+ * Return: prints anything.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	int count = 0;
+	char *string, *separator;
+
+	va_start(ap, format);
+
+	separator = "";
+
+	while (format && *(format + count))
+	{
+		switch (*(format + count))
+		{
+			case  'c':
+				printf("%s%c", separator, va_arg(ap, int));
+					break;
+			case 'i':
+				printf("%s%d", separator, va_arg(ap, int));
+					break;
+			case 'f':
+				printf("%s%f", separator, va_arg(ap, double));
+				break;
+			case 's':
+				string = va_arg(ap, char *);
+				if (string == NULL)
+					printf("(nil)");
+				else
+					printf("%s%s", separator, string);
+				break;
+			default:
+				count++;
+				continue;
+		}
+		separator = ", ";
+		count++;
+	}
+	va_end(ap);
+	printf("\n");
+}
