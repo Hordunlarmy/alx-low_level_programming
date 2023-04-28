d8bfecf3a00a547e233aa3b95008ef1e0b08e152
Hordunlarmy <Hordunlarmy@gmail.com>
2023-04-28 15:09:31 +0100

    Add a program to print a poem and a quote upon start-up using constructor attribute

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * hare_and_tortoise - Entry point
+ *
+ * Return: success
+ */
+void __attribute__((constructor)) hare_and_tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+
+}
