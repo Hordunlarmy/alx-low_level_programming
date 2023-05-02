9b3cb17c163f75d3ef2660479207f553c8a0b2df
Hordunlarmy <Hordunlarmy@gmail.com>
2023-05-02 22:13:48 +0100

    Add free_listint2 function to free a listint_t list, updating the header file accordingly

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Entry point
+ * @head: pointer to pointer to head of list
+ * Return: frees a listint_t list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	if (*head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		ptr = *head;
+		*head = (*head)->next;
+		free(ptr);
+	}
+	head = NULL;
+}
