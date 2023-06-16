65dbde08eb35617019a06f09065c2513d681e110
Hordunlarmy <Hordunlarmy@gmail.com>
2023-06-16 21:27:20 +0100

    Add script to find largest palindrome of 3-digit numbers

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -4,7 +4,7 @@
  * delete_dnodeint_at_index -  a function that deletes the
  *                   node at index index of a dlistint_t linked list.
  * @head: pointer to head pointer
- * @index: nth node 
+ * @index: nth node
  * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
