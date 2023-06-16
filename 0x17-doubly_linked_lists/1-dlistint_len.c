f7dd0f205c886b9a2cc9b672f635c760e7fa3665
Hordunlarmy <Hordunlarmy@gmail.com>
2023-06-16 10:49:50 +0100

    Return length of doubly linked list recursively

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,23 +1,27 @@
 #include "lists.h"
 
 /**
- * dlistint_len -  a function that returns the number
- *               of elements in a linked dlistint_t list.
- * @h: pointer to head of list
- * Return: number of elements
- */
+* dlistint_len -  a function that returns the number
+*               of elements in a linked dlistint_t list.
+* @h: pointer to head of list
+* Return: number of elements
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *temp = h;
-	size_t r_value = 0;
 
-	if (h == NULL)
+	if (temp == NULL)
 		return (0);
 
-	while (temp != NULL)
-	{
-		temp = temp->next;
-		r_value++;
-	}
-	return (r_value);
+	/**
+	* while (temp != NULL)
+	*{
+	*	temp = temp->next;
+	*	r_value++;
+	*}
+	*return (r_value);
+	*/
+
+	else
+		return (1 + dlistint_len(temp->next));
 }
