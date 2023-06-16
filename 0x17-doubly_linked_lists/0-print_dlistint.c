94956c1e2f6bcdd3e343fd73311d631a4e18b063
Hordunlarmy <Hordunlarmy@gmail.com>
2023-06-16 10:28:51 +0100

    Add doubly linked list implementation

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: head pointer to list
+ * Return: size of list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t r_value = 0;
+	const dlistint_t *temp = h;
+
+	if (h == NULL)
+		return (0);
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		r_value++;
+	}
+	return (r_value);
+}
