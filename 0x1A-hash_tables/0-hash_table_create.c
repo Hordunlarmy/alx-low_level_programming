69fd75402419f4bc9e7c5e067ca575f74bd42057
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-08 13:45:37 +0100

    Create hash table with given size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,31 +3,31 @@
 /**
  * hash_table_create - A function that creates a hash table
  * @size: The size of the array
- * Returns: A hash table.
+ * Return: A hash table.
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table_created;
-    unsigned long int i = 0;
+	hash_table_t *hash_table_created;
+	unsigned long int i = 0;
 
-    hash_table_created = malloc(sizeof(hash_table_t));
+	hash_table_created = malloc(sizeof(hash_table_t));
 
-    if (hash_table_created == NULL)
-        return (NULL);
+	if (hash_table_created == NULL)
+		return (NULL);
 
-    hash_table_created->size = size;
-    hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
+	hash_table_created->size = size;
+	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
 
-    if (hash_table_created->array == NULL)
-        return (NULL);
+	if (hash_table_created->array == NULL)
+		return (NULL);
 
-    while (i < size)
-    {
-        hash_table_created->array[i] = NULL;
-        i++;
-    }
+	while (i < size)
+	{
+		hash_table_created->array[i] = NULL;
+		i++;
+	}
 
-    return (hash_table_created);
+	return (hash_table_created);
 }
 
