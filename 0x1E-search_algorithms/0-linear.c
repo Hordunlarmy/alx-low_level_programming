c61fb359682a7d5fa3ac639360caa12929259f73
hordunlarmy <hordunlarmy@gmail.com>
2023-12-13 09:55:38 +0000

    Implement linear and binary search algorithms in C

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,14 +11,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+	size_t i;
 
-    for (i = 0; i < size; i++) {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
+	for (i = 0; i < size; i++)
 
-    return (-1);
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
 }
-
